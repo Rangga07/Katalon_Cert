80303786389993baa108b077d941d5958912e513
M Rangga Hendarmin <rangga.hendarmin@pactindo.com>
2024-08-20 21:48:13 +0700

    Update test suite

---
+++ Test Suites/TS01 - Regression Appoinment.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01 - Regression Appoinment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>43075ba7-4ec3-43d3-8e9e-ded1ad6eaf2c</testSuiteGuid>
+   <testCaseLink>
+      <guid>cdd53b27-3a4a-466f-b2f0-119b96a6e3e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/TC - Login/TC01 - Login Success</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe1e8caf-b851-4b8a-a57d-d8bb27466eed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEB/TC - Appoinment/TC02 - Make Appointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>15de6493-e0e9-4233-879b-2b72ca5a7a76</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
